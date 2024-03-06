3bd8cbd41d3597cb4b6b8435a2bdc1a01d9c40a1
kabandapatrick2580 <sendsome2019@gmail.com>
2024-03-06 16:45:15 +0200

    slight chnges

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,12 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => [resolve, reject]);
+// 0-promise.js
+
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Simulate an asynchronous operation (e.g., API request)
+      setTimeout(() => {
+        resolve("Response from API");
+      }, 1000); // Simulated delay of 1 second
+    });
   }
+  
+  export default getResponseFromAPI;
