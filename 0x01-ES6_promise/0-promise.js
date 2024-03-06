a4b8cd16e41070d0d81dc414ddaf7c8274cd2968
kabandapatrick2580 <sendsome2019@gmail.com>
2024-03-06 16:48:53 +0200

    1-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,13 @@
-function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Response from API");
-      }, 1000);
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
     });
   }
-  
-  export default getResponseFromAPI;
   
