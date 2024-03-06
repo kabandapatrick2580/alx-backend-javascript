694b24b404b06da0964a003c386f981fba2d00c8
kabandapatrick2580 <sendsome2019@gmail.com>
2024-03-06 17:02:22 +0200

    7-load_balancer.js

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,8 @@
+function uploadPhoto(fileName) {
+    return new Promise((resolve, reject) => {
+      reject(new Error(`${fileName} cannot be processed`));
+    });
+  }
+  
+  export default uploadPhoto;
+  
